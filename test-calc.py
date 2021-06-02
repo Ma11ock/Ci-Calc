d0c2126afa1021067838ec15181bab962b695d18
Ryan Jeffrey <ryan@ryanmj.xyz>
2021-06-01 17:19:36 -0700

    multiply, divide

--- test-calc.py
+++ test-calc.py
@@ -4,7 +4,7 @@
 Test for calculator app.
 """
 
-from calc import add,subtract
+from calc import add,subtract,multiply,divide
 
 
 class TestCalcApp:
@@ -13,3 +13,12 @@ class TestCalcApp:
 
     def test_subtract(self):
         assert 5 == subtract(7, 2)
+        
+    def test_add2(self):
+        assert 7 == add(add(3, 2), 2)
+
+    def test_mult(self):
+        assert 12 == multiply(3, 4)
+
+    def test_divide(self):
+        assert 12 == divide(144, 12)
